b96340d7cc491ff01fc39ddf0df0774716326fa8
Choi ho jin <jkhh9725@gmail.com>
2023-06-07 12:31:59 +0900

    component

--- personal-study/src/components/Test.jsx
+++ personal-study/src/components/Test.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-import "./Test.css";
-
-function test() {
-  return <div>hello</div>;
-}
-
-export default test;
+const Test = () => {
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .then((res) => {
+        setPost(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+export default Test;
+// Parent component
