def03edd52aace5e42cb9fe2a38661f55310b7bf
Choi ho jin <jkhh9725@gmail.com>
2023-06-19 22:48:26 +0900

    메인 페이지

--- personal-study/src/pages/MainPage.jsx
+++ personal-study/src/pages/MainPage.jsx
@@ -22,6 +22,7 @@ import ExpandingSearchInputPage from "./ExpandingSearchInputPage";
 import ColorsPage from "./ColorsPage";
 import ModalPage from "./ModalPage";
 import Button from "../components/Button";
+import UiComponent from "../components/UiComponent";
 
 function MainPage({ component }) {
   // uiInfo 보여주는 상태값
@@ -30,12 +31,12 @@ function MainPage({ component }) {
   const handleClickOpenUiPages = () => {
     setIsUiInfo((prev) => !prev);
   };
-
+  const Title = "UI Challenge 50";
   return (
     <div css={mainPageCss}>
       <div className="headLine">
         <div css={headLine}>
-          <h1>UI Challenge 50</h1>
+          <h1>{Title}</h1>
         </div>
         <div css={btnHeader}>
           <Button onClick={handleClickOpenUiPages}>
@@ -45,13 +46,11 @@ function MainPage({ component }) {
       </div>
       <div css={contentsCss}>
         <div css={uiInfoHeaderCss(isUiInfo)}>
-          {/* 여기에는 page를 넣을 거야 */}
           <UiPageLink />
         </div>
         <div css={pageInfoHeaderCss(isUiInfo)}>
-          <div>
-            {component}
-            여기서 어떠한 값을 받아서 그 값에 따라 화면을 바꿔야 할 거 같음.
+          <div css={pageContainerCss}>
+            <UiComponent />
           </div>
         </div>
       </div>
@@ -74,7 +73,7 @@ const headLine = css`
   background-color: #000000;
   color: #ffffff;
   font-family: Source Sans Pro, Helvetica Neue, sans-serif;
-  font-size: 24px;
+  font-size: 36px;
   letter-spacing: 0.2rem;
   padding: 20px;
   height: 70px;
@@ -89,6 +88,7 @@ const btnHeader = css`
 const contentsCss = css`
   display: flex;
 `;
+// icon 모음
 const uiInfoHeaderCss = (isUiInfo) => css`
   width: ${isUiInfo ? "300px" : "0px"};
   height: 100vh;
@@ -97,12 +97,21 @@ const uiInfoHeaderCss = (isUiInfo) => css`
   overflow-x: scroll;
   /* ------ 기본 스택 ------ */
 `;
-
+// page 보여주는 화면
 const pageInfoHeaderCss = (isUiInfo) => css`
-  width: ${isUiInfo ? "80rem" : "100rem"};
+  width: 100%; /* ${isUiInfo ? "80rem" : "100rem"}; */
   height: 100vh;
   background-color: #000000;
+  padding: 30px 20px;
   transition: width 0.2s cubic-bezier(0.7, 0, 0.3, 1);
   /* ------ 기본 스택 ------ */
   color: white;
+`;
+
+const pageContainerCss = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
 `;
